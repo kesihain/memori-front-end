62a50f546b8c8d6d9be8939ecaacf4b4850d0288
Rachel Wibisana <rachelwibisana@Rachels-MacBook-Pro-3.local>
2020-08-18 17:02:13 +0800

    homepage buttons

--- assets/screens/Home.js
+++ assets/screens/Home.js
@@ -1,17 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image, SafeAreaView, Button } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView, Button, TouchableOpacity, Alert } from 'react-native';
+import AwesomeButtonRick from 'react-native-really-awesome-button/src/themes/rick';
 
-  function HomeScreen(){
+  function HomeScreen({navigation}){
     return (
       <SafeAreaView style={styles.container}>
-        <Image
-            style={{width: 500, height: 500}}
-            source={{uri: 'https://media.giphy.com/media/JotKsFe1MXCWcq7kTT/giphy.gif'}} />
-        <StatusBar style="auto" />
+          <Image
+              style={{width: 500, height: 500}}
+              source={{uri: 'https://media.giphy.com/media/JotKsFe1MXCWcq7kTT/giphy.gif'}} />
+          <View style={styles.btns} >
+              <AwesomeButtonRick 
+              onPress={() => navigation.navigate("locations")} 
+              type="secondary">
+              Make
+              </AwesomeButtonRick>
+              <AwesomeButtonRick 
+              onPress={() => navigation.navigate("locations")} 
+              type="secondary">
+              Remember
+              </AwesomeButtonRick>
+          </View>
+          <View>
+              <AwesomeButtonRick 
+              onPress={() => navigation.navigate("locations")} 
+              type="secondary">
+              Locations
+              </AwesomeButtonRick>
+          </View>
+          <StatusBar style="auto" />
       </SafeAreaView> 
-      
-
     );
 
     }
@@ -21,6 +39,10 @@ import { StyleSheet, Text, View, Image, SafeAreaView, Button } from 'react-nativ
         backgroundColor: '#ffe6dd',
         alignItems: 'center',
         justifyContent: 'center',
+      },
+      btns: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
       },
     });
     export default HomeScreen
--- assets/screens/LocationsScreen.js
+++ assets/screens/LocationsScreen.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-export default function LocationsScreen() {
+export default function LocationsScreen({navigation}) {
   return (
     <View>
       <Text></Text>
