5aaacd2de11d8199c9c009ce452f47c246c2b45d
kesihain <kesihainselvarajoo@gmail.com>
2020-08-19 12:21:55 +0800

    Added location screen functionalities (list and form)

--- assets/components/LocationForm.js
+++ assets/components/LocationForm.js
@@ -2,10 +2,21 @@ import React,{useState, useEffect} from 'react';
 import { StyleSheet, Text,TextInput,Button,Alert, View } from "react-native";
 import {useForm} from "react-hook-form";
 
-export default function LocationForm(){
+export default function LocationForm({location,setLocation}){
+    const [text,setText] = useState('')
+    const [id,setId] = useState(location[location.length-1].id+1) //might need some change when using back-end api
     const onSubmit = data => {
-        console.log('test')
         Alert.alert("Form Data", data);
+        if (data.location != '' && !Object.values(location).includes(data.location)){
+            setId(id+1);
+            setLocation([
+                ...location,
+                {id:id, name:data.location}
+            ]);
+            setValue('location',"");
+            setText("");
+        }
+        console.log(location)
     };
     const {register,handleSubmit,setValue} = useForm();
 
@@ -16,7 +27,7 @@ export default function LocationForm(){
     return (
         <View style={styles.container}>
             <Text style={styles.text}>Type in location</Text>
-            <TextInput style={styles.input} onChangeText={text=>{setValue('location',text)}}></TextInput>
+            <TextInput style={styles.input} value={text} onChangeText={text=>{setValue('location',text);setText(text)}}></TextInput>
             <Button title='Submit' color='black' onPress={handleSubmit(onSubmit)}/>
         </View>    
     )
--- assets/screens/LocationsScreen.js
+++ assets/screens/LocationsScreen.js
@@ -1,12 +1,17 @@
 import React,{useState} from "react";
 import { StyleSheet, Text, View } from "react-native";
 import LocationForm from '../components/LocationForm'
+import LocationList from "../components/LocationList";
 
 export default function LocationsScreen({navigation}) {
+  const [location,setLocation] = useState([
+      { id: 1, name: 'Next Academy' }
+  ])
   return (
     <View>
       <Text onPress={() => navigation.navigate("home")}>Home</Text>
-      <LocationForm></LocationForm>
+      <LocationForm location={location} setLocation={setLocation}></LocationForm>
+      <LocationList location={location}></LocationList>
     </View>
   );
 }
