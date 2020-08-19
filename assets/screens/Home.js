8995ea0de689e9cb1cf92d3cea5cba2c2aa222db
hilman <hilmanbinzurin@gmail.com>
2020-08-19 12:29:41 +0800

    edited location screen

--- assets/screens/Home.js
+++ assets/screens/Home.js
@@ -1,26 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, Image, SafeAreaView, Button } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  SafeAreaView,
+  Button,
+} from "react-native";
 
-  function HomeScreen(){
-    return (
-      <SafeAreaView style={styles.container}>
-        <Image
-            style={{width: 500, height: 500}}
-            source={{uri: 'https://media.giphy.com/media/JotKsFe1MXCWcq7kTT/giphy.gif'}} />
-        <StatusBar style="auto" />
-      </SafeAreaView> 
-      
+function HomeScreen() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Image
+        style={{ width: 500, height: 500 }}
+        source={{
+          uri: "https://media.giphy.com/media/JotKsFe1MXCWcq7kTT/giphy.gif",
+        }}
+      />
+      <StatusBar style="auto" />
 
-    );
-
-    }
-    const styles = StyleSheet.create({
-      container: {
-        flex: 1,
-        backgroundColor: '#ffe6dd',
-        alignItems: 'center',
-        justifyContent: 'center',
-      },
-    });
-    export default HomeScreen
+      <Text>hello there</Text>
+    </SafeAreaView>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#ffe6dd",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+export default HomeScreen;
--- assets/screens/LocationsScreen.js
+++ assets/screens/LocationsScreen.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from "react-native";
 export default function LocationsScreen({ navigation }) {
   return (
     <View>
-      <Text onPress={() => navigation.navigate("home")}>wuelalalalawp</Text>
+      <Text onPress={() => navigation.navigate("home")}>go back home</Text>
+      <Text>set ur locations!</Text>
     </View>
   );
 }
